0c56bd7db36c6cbad51fc15282f59cd98eb006c6
fcolavini <facundo.colavini@cedeira.com.ar>
2025-01-15 11:47:21 -0300

    First commit

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,32 +1,37 @@
 import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
+import MarkdownIt from 'markdown-it';
+import sanitizeHtml from 'sanitize-html'; // Importar correctamente sanitize-html
 
-/* 
-Es un archivo que se encarga de generar un archivo XML con los datos de los posts del blog.
-*/
+const parser = new MarkdownIt();
 
-export const GET: APIRoute = async ({ params, request,site }) => {
-    const blogPosts = await getCollection('blog');
-   
-    return rss({
-        // stylesheet: '/styles/rss.xsl', // Estilos del rss
-        // `<title>` field in output xml
-        title: 'Facundo Blog',
-        // `<description>` field in output xml
-        description: 'Un simple blog sobre mis aventuras con Astro',
-        // Pull in your project "site" from the endpoint context
-        // https://docs.astro.build/en/reference/api-reference/#site
-        site: site ?? '',
-        // Array of `<item>`s in output xml
-        // See "Generating items" section for examples using content collections and glob imports
-        // (optional) inject custom xml
-        items: blogPosts.map(({data, filePath }) => ({
-            title: data.title,
-            pubDate: data.date,
-            description: data.description,
-            link: `posts/${filePath?.split('/').slice(-1)[0].replace('.md', '')}`
-        })),
-        customData: `<language>es-ar</language>`,
-      });
-}
+export const GET: APIRoute = async ({ params, request, site }) => {
+  const blogPosts = await getCollection('blog');
+
+  return rss({
+    title: 'Facundo Blog',
+    description: 'Un simple blog sobre mis aventuras con Astro',
+    site: site ?? '',
+    items: blogPosts.map(({ data, filePath, body }) => ({
+      title: data.title,
+      pubDate: data.date,
+      description: data.description,
+      xmlns: {
+        media: 'http://search.yahoo.com/mrss/',
+      },
+      link: `posts/${filePath?.split('/').slice(-1)[0].replace('.md', '')}`,
+      content: sanitizeHtml(parser.render(body || ''), {
+        allowedTags: ['b', 'i', 'em', 'strong', 'a', 'img'], // Etiquetas permitidas
+      }),
+      customData: `<media:content
+        type="image/${data.image.format === 'jpg' ? 'jpeg' : 'png'}"
+        width="${data.image.width}"
+        height="${data.image.height}"
+        medium="image"
+        url="${site + data.image.src}" />
+      `,
+    })),
+    customData: `<language>es-ar</language>`,
+  });
+};
